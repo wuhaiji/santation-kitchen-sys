ee7dfa6679a6ccefce3dd5bf41015a31e4e9da65
whj <1475351569@qq.com>
2020-12-14 20:00:09 +0800

    [fix]车辆类型

--- src/main/java/com/yuntun/sanitationkitchen/controller/VehicleTypeController.java
+++ src/main/java/com/yuntun/sanitationkitchen/controller/VehicleTypeController.java
@@ -153,7 +153,7 @@ public class VehicleTypeController {
         //查询车辆类型下面是否有车辆
         List<Vehicle> vehicles = iVehicleService.list(new QueryWrapper<Vehicle>().eq("sanitation_office_id", uid));
         if(EptUtil.isNotEmpty(vehicles)){
-            throw new ServiceException(VehicleCode.NUMBER_PLATE_ALREADY_EXISTS);
+            throw new ServiceException(VehicleCode.HAS_VEHICLE_LIST);
         }
         ErrorUtil.isObjectNull(uid, "uid");
 
