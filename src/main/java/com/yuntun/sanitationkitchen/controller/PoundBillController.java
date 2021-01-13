267036c6a939b8cc6762249b2779c85e90fd5ad7
me <f-kite@163.com>
2021-01-13 16:10:06 +0800

    [feat]主页接口

--- src/main/java/com/yuntun/sanitationkitchen/controller/PoundBillController.java
+++ src/main/java/com/yuntun/sanitationkitchen/controller/PoundBillController.java
@@ -5,14 +5,11 @@ import com.yuntun.sanitationkitchen.auth.Limit;
 import com.yuntun.sanitationkitchen.model.dto.PoundBillDto;
 import com.yuntun.sanitationkitchen.model.response.Result;
 import com.yuntun.sanitationkitchen.service.IPoundBillService;
-import com.yuntun.sanitationkitchen.util.ErrorUtil;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -27,47 +24,59 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class PoundBillController {
 
-    @Autowired
-    IPoundBillService iPoundBillService;
+  @Autowired
+  IPoundBillService iPoundBillService;
 
-    /**
-     * 地磅榜单 下拉框
-     *
-     * @author wujihong
-     * @since 2020-12-02 11:21
-     */
-    @Limit("data:poundBill:query")
-    @RequestMapping("/option")
-    public Result selectPoundBillOption() {
-        return Result.ok(iPoundBillService.selectPoundBillOption());
-    }
+  /**
+   * 地磅榜单 下拉框
+   *
+   * @author wujihong
+   * @since 2020-12-02 11:21
+   */
+  @Limit("data:poundBill:query")
+  @RequestMapping("/option")
+  public Result selectPoundBillOption() {
+    return Result.ok(iPoundBillService.selectPoundBillOption());
+  }
 
-    /**
-     * 分页查询地磅磅单
-     *
-     * @param poundBillDto
-     * @return
-     */
-    @Limit("data:poundBill:query")
-    @RequestMapping("/list")
-    public Result list(PoundBillDto poundBillDto) {
-        return Result.ok(iPoundBillService.pagePoundBill(poundBillDto));
-        //return Result.ok(iPoundBillService.findPoundBillList(poundBillDto));
-    }
+  /**
+   * 分页查询地磅磅单
+   *
+   * @param poundBillDto
+   * @return
+   */
+  @Limit("data:poundBill:query")
+  @RequestMapping("/list")
+  public Result list(PoundBillDto poundBillDto) {
+    return Result.ok(iPoundBillService.pagePoundBill(poundBillDto));
+    //return Result.ok(iPoundBillService.findPoundBillList(poundBillDto));
+  }
 
-    /**
-     * 用excel的形式导出榜单
-     *
-     * @param poundBillDto
-     * @param response
-     */
-    @Limit("data:poundBill:export")
-    @RequestMapping(value = "/export")
-    public void export(PoundBillDto poundBillDto, HttpServletResponse response) {
-        System.out.println("poundBillDto:"+poundBillDto);
-        iPoundBillService.exportPoundBill(poundBillDto, response);
-    }
 
+  /**
+   * 分页查询地磅磅单
+   *
+   * @param poundBillDto
+   * @return
+   */
+  @Limit("data:poundBill:query")
+  @RequestMapping("/date")
+  public Result getDateTotal(PoundBillDto poundBillDto) {
+    return Result.ok(iPoundBillService.getDateTotal(poundBillDto));
+  }
+
+  /**
+   * 用excel的形式导出榜单
+   *
+   * @param poundBillDto
+   * @param response
+   */
+  @Limit("data:poundBill:export")
+  @RequestMapping(value = "/export")
+  public void export(PoundBillDto poundBillDto, HttpServletResponse response) {
+    System.out.println("poundBillDto:" + poundBillDto);
+    iPoundBillService.exportPoundBill(poundBillDto, response);
+  }
 
 
 }
